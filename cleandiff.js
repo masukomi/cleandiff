dc601b7da2d09e1c6d44175875107e28928df304
masukomi <masukomi@masukomi.org>
2016-03-03 08:27:54 -0500

    now auto-expands around changed lines

--- cleandiff.js
+++ cleandiff.js
@@ -1,17 +1,32 @@
-function getChildren(n, skipMe){
+function getChildren(node, skipMe){
 	var r = [];
-	for ( ; n; n = n.nextSibling ) 
-	   if ( n.nodeType == 1 && n != skipMe)
-		  r.push( n );		
+	for ( ; node; node = node.nextSibling ) 
+	   if ( node.nodeType == 1 && node != skipMe)
+		  r.push( node );		
 	return r;
 };
 
-function getChildrenUpTo(n, stopAt){
+function getChildrenUpTo(node, stopAt){
 	var r = [];
-	for ( ; n; n = n.nextSibling ) {
-		if ( n.nodeType == 1 && n != stopAt) {
-			r.push( n );
-		} else if ( n == stopAt) {
+	for ( ; node; node = node.nextSibling ) {
+		if ( node.nodeType == 1 && node != stopAt) {
+			if (node.classList[0] == "line") {
+			// ^^ only to compensate for bug in bash version's source html
+				r.push( node );
+			}
+		} else if ( node == stopAt) {
+			break;
+		}
+	}
+	return r;
+}
+function getLaterNSiblings(node, n){
+	var r = [];
+	for ( ; node; node = node.nextSibling ) {
+		if ( r.length < n && node.nodeType == 1) {
+			// nodeType 3 is Text 1 is element. 
+			r.push( node )
+		} else if (r.length == n) {
 			break;
 		}
 	}
@@ -30,11 +45,22 @@ function getPreviousNSiblings(node, count){
 	return getEarlierSiblings(node).slice((count * -1));
 }
 
-function expandPreviousSiblings(node){
-	var predecessors = getPreviousNSiblings(node, 20);
+function expandPreviousSiblings(node, how_many){
+	var predecessors = getPreviousNSiblings(node, how_many);
 	for (var x = predecessors.length -1; x > -1 ; x--){
-		if (predecessors[x].className.indexOf("unchanged") > -1) {
-			predecessors[x].className = predecessors[x].className + " expanded";
+		expandNodeIfUnchanged(predecessors[x]);
+	}
+}
+function expandLaterSiblings(node, how_many){
+	var antecessors = getLaterNSiblings(node, how_many);
+	for (var x = 0; x < antecessors.length; x++){
+		expandNodeIfUnchanged( antecessors[x])
+	}
+}
+function expandNodeIfUnchanged(node){
+	if (typeof(node) != "undefined"){
+		if (node.className.indexOf("unchanged") > -1) {
+			node.className = node.className + " expanded";
 		}
 	}
 }
@@ -43,11 +69,35 @@ function getAllAnchors(){
 	return document.getElementsByClassName("linenum_link");
 }
 
-function activateAnchors(){
-	var number_anchors = getAllAnchors();
+function getChangedLineDivs(){
+	var all_lines = document.getElementsByClassName("line");
+	var changed_lines = [];
+	for (var x = 0; x < all_lines.length; x++){
+		var node = all_lines[x];
+		if (node.className.indexOf("unchanged") == -1) {
+			changed_lines.push(node);
+		}
+	}
+	return changed_lines;
+}
+function addOnClickToAnchors(number_anchors){
 	for (var x = 0; x < number_anchors.length; x++){
 		var anchor = number_anchors[x]
 		anchor.addEventListener("click",
-			function(){expandPreviousSiblings(this.parentNode.parentNode)}, false);
+			function(){expandPreviousSiblings(this.parentNode.parentNode, 20)}, false);
 	}
+}
+function expandAroundChangedLines(){
+	var changed_lines = getChangedLineDivs();
+	for (var x = 0; x < changed_lines.length; x++){
+		var node = changed_lines[x]
+		expandPreviousSiblings(node, 5);
+		expandLaterSiblings(node, 5);
+	}
+}
+
+function activateAnchors(){
+	var number_anchors = getAllAnchors();
+	addOnClickToAnchors(number_anchors);
+	expandAroundChangedLines()
 }
